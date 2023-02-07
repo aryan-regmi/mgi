8584d004b3417ddf5f20ad0901e5e82ed4ea6a46
Aryan Regmi <arre9260@protonmail.com>
2023-02-06 17:39:19 -0700

    Turned into library & added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,147 @@
-pub mod game;
+use std::time::Duration;
+
+use sdl2::Sdl;
+
+pub use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::Canvas, video::Window};
+
+fn find_sdl_gl_driver() -> Option<u32> {
+    for (index, item) in sdl2::render::drivers().enumerate() {
+        if item.name == "opengl" {
+            return Some(index as u32);
+        }
+    }
+    None
+}
+
+pub trait BaseGame {
+    fn update(&mut self);
+    fn render(&mut self, canvas: &mut Canvas<Window>);
+
+    fn handle_events(&mut self, event: Event);
+    fn is_running(&self) -> bool;
+}
+
+pub trait Entity {
+    fn update(&mut self);
+    fn render(&mut self, canvas: &mut Canvas<Window>);
+
+    fn id(&self) -> u32;
+    fn position(&self) -> (f64, f64);
+    fn velocity(&self) -> (f64, f64);
+
+    /// Checks if two entities are the same
+    fn is_same(&self, other: &dyn Entity) -> bool {
+        self.id() == other.id()
+    }
+}
+
+pub struct GameBuilder<'a> {
+    #[allow(dead_code)]
+    size: (u32, u32),
+
+    sdl_ctx: Sdl,
+
+    canvas: Canvas<Window>,
+
+    game_obj: &'a mut dyn BaseGame,
+}
+
+impl<'a> GameBuilder<'a> {
+    // TODO: Proper error handling!!
+    pub fn init(title: &str, width: u32, height: u32, game_obj: &'a mut dyn BaseGame) -> Self {
+        // Initialize SDL
+        let sdl_ctx = sdl2::init().unwrap();
+        let video_sys = sdl_ctx.video().unwrap();
+        let window = video_sys
+            .window(title, width, height)
+            .opengl()
+            .build()
+            .unwrap();
+
+        Self {
+            size: (width, height),
+
+            sdl_ctx,
+
+            canvas: window
+                .into_canvas()
+                .index(find_sdl_gl_driver().unwrap())
+                .build()
+                .unwrap(),
+
+            game_obj,
+        }
+    }
+
+    pub fn run(&mut self) {
+        let mut events = self.sdl_ctx.event_pump().unwrap();
+        while self.game_obj.is_running() {
+            self.canvas.clear();
+
+            // Event handling
+            for event in events.poll_iter() {
+                self.game_obj.handle_events(event);
+            }
+
+            // Update
+            self.game_obj.update();
+
+            // Render
+            self.game_obj.render(&mut self.canvas);
+
+            // TODO: Replace 60 by fps
+            std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{BaseGame, Canvas, Color, Event, GameBuilder, Keycode, Window};
+    use std::error::Error;
+
+    struct TestGame {
+        running: bool,
+    }
+
+    impl TestGame {
+        pub fn init() -> Self {
+            Self { running: true }
+        }
+    }
+
+    impl BaseGame for TestGame {
+        fn update(&mut self) {}
+
+        fn render(&mut self, canvas: &mut Canvas<Window>) {
+            canvas.set_draw_color(Color::WHITE);
+            canvas.clear();
+            canvas.present();
+        }
+
+        fn is_running(&self) -> bool {
+            self.running
+        }
+
+        fn handle_events(&mut self, event: sdl2::event::Event) {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => self.running = false,
+                _ => (),
+            }
+        }
+    }
+
+    #[test]
+    fn run_game() -> Result<(), Box<dyn Error>> {
+        const WIDTH: u32 = 1280;
+        const HEIGHT: u32 = 720;
+
+        GameBuilder::init("TEST", WIDTH, HEIGHT, &mut TestGame::init()).run();
+
+        Ok(())
+    }
+}
